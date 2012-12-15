1d004b3dd0d34f8422970a4be1f24972015b994d
Davide Bellettini <davide.bellettini@gmail.com>
2012-12-15 13:13:34 +0100

    Faces

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$photos = array (
+    'Bob' => 'Bob.jpg'
+);
+
+$charset = 'utf8';
+header('Content-type: text/html; charset='.$charset);
+?>
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Corso PHP UniversiBO - Esercizio 1</title>
+  </head>
+  <body>
+    <h1>Corso di PHP - Esercizio 1</h1>
+  </body>
+</html>
